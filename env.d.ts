7a38d53c2acce3cfb12b338a9ac32e12a02df87a
External Host <haydenr261@gmail.com>
2026-05-05 14:47:31 -0700

    environment configuration

--- env.d.ts
+++ env.d.ts
@@ -1,7 +1,7 @@
 interface ImportMetaEnv {
-	readonly PUBLIC_CLOUDINARY_NAME: string;
-    readonly CLOUDINARY_API_KEY: string;
-    readonly CLOUDINARY_API_SECRET: string;
+	readonly PUBLIC_CLOUDINARY_CLOUD_NAME: string;
+	readonly PUBLIC_CLOUDINARY_API_KEY: string;
+	readonly CLOUDINARY_API_SECRET: string;
 }
 
 interface ImportMeta {
